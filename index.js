6fe04421650a0851c7f1214370a1bce55343c82b
Asjdnnc <aditya05yt@gmail.com>
2025-01-15 10:51:05 +0530

    fixed origin url for frontend

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json()); // Parse JSON request bodies
 app.use(express.urlencoded({extended:true}));
 //for connecting fronted and backend url
 app.use(cors({
-    origin: "https://marvel-w8vq.onrender.com",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 
