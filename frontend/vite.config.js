5697dc03b2b868be6d8d01e7b563372cb272c4ff
Asjdnnc <aditya05yt@gmail.com>
2025-01-15 12:36:29 +0530

    frontend deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // Proxy all requests starting with /api to the backend server
+      '/api': {
+        target: 'https://marvel-w8vq.onrender.com', // Your backend URL
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
