9a9eb0b6bfe9d95125a322c941175c634610ec9b
Asjdnnc <aditya05yt@gmail.com>
2025-01-28 23:31:54 +0530

    responsive

--- frontend/src/pages/MovieDetails.jsx
+++ frontend/src/pages/MovieDetails.jsx
@@ -1,63 +1,73 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import "./MovieDetails.css";
-import "../components/Trailer.css"
-import Trailer from "../components/Trailer"
-import Music from "../components/Music"
+import "../components/Trailer.css";
+import Trailer from "../components/Trailer";
+import Music from "../components/Music";
 import Related from "../components/Related";
 import axios from "axios";
+
 export default function MovieDetails() {
   const { id } = useParams();
   const [movie, setMovie] = useState(null);
-  const [loading,setloading] = useState(true);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     console.log("movie called");
     axios.get(`https://marvel-w8vq.onrender.com/api/movies/${id}`).then((response) => {
       setMovie(response.data);
-      setloading(false);
-    }).catch((err) => {console.error("Error fetching movies details:", err);})
-    }, [id]);
-  if (loading){return <div style={{minHeight:"100vh",minWidth:"200vh", backgroundColor:"black",paddingTop:"200px",paddingLeft:"330px",height:"500px",width:"800px"}}>
-    <img src="https://cdn.dribbble.com/users/29051/screenshots/1671982/loading-anim-dribbble.gif" style={{paddingLeft:"150px",height:"500px",width:"800px"}}/>;
-    <p style={{color:"white",fontSize:"40px",marginLeft:"20%"}}>loading...</p>
-    </div>}
-    return(
-    <div style={{backgroundColor:"black" ,color:"white",marginBottom:"0px"} }>
-      <div>
-      <img src={movie.img2} alt={movie.title} 
-      style={{width: "100%",height: "auto" }} />
-      <div>
-        <img className="image" src={movie.poster_path} style={{
-          height:"400px",
-          width:"300px",
-          position:"absolute",
-          left:"70%",
-          top:"45%",
-          boxShadow:"20px"
-        }}></img>
+      setLoading(false);
+    }).catch((err) => { console.error("Error fetching movie details:", err); });
+  }, [id]);
+
+  if (loading) {
+    return (
+      <div style={{
+        minHeight: "100vh", minWidth: "200vh", backgroundColor: "black",
+        paddingTop: "200px", paddingLeft: "330px", height: "500px", width: "800px"
+      }}>
+        <img src="https://cdn.dribbble.com/users/29051/screenshots/1671982/loading-anim-dribbble.gif"
+          style={{ paddingLeft: "150px", height: "500px", width: "800px" }} />
+        <p style={{ color: "white", fontSize: "40px", marginLeft: "20%" }}>loading...</p>
       </div>
-      <div style={{marginLeft:"3%",paddingBottom:"4px",width:"60%",fontSize:"20px"} }>
-      <h1>{movie.title}</h1>
-      <p style={{color:"gray"}}>{movie.overview}</p>
-      <p>Rating: {movie.vote_average} <i className="fa-solid fa-star" style={{color:"#e8b10c"}}></i></p>
-      <p>Director: {movie.director}</p>
-      Cast: 
-      <ul>
-      {movie.cast[0].map((actor, index) => (
-      <li key={index}>{actor}</li>
-      ))}
-      </ul>
-      <p>Release Date: {`${new Date(movie.release_date).toLocaleString('default', { month: 'long' })} ${new Date(movie.release_date).getFullYear()}`}</p>
-      <p>Runtime: {movie.runtime} mins</p>
-      <p>Budget: ${movie.budget.toLocaleString()}</p>
-      <p>Revenue: ${movie.revenue.toLocaleString()}</p>
+    );
+  }
+
+  return (
+    <div style={{ backgroundColor: "black", color: "white", marginBottom: "0px" }}>
+      <div>
+        <img src={movie.img2} alt={movie.title}
+          style={{ width: "100%", height: "auto" }} />
+        <div>
+          <img className="image" src={movie.poster_path} style={{
+            height: "400px", width: "300px", position: "absolute", left: "70%", top: "45%",
+            boxShadow: "20px"
+          }} alt={movie.title} />
+        </div>
+        <div style={{
+          marginLeft: "3%", paddingBottom: "4px", width: "60%", fontSize: "20px"
+        }}>
+          <h1>{movie.title}</h1>
+          <p style={{ color: "gray" }}>{movie.overview}</p>
+          <p>Rating: {movie.vote_average} <i className="fa-solid fa-star" style={{ color: "#e8b10c" }}></i></p>
+          <p>Director: {movie.director}</p>
+          Cast:
+          <ul>
+            {movie.cast[0].map((actor, index) => (
+              <li key={index}>{actor}</li>
+            ))}
+          </ul>
+          <p>Release Date: {`${new Date(movie.release_date).toLocaleString('default', { month: 'long' })} ${new Date(movie.release_date).getFullYear()}`}</p>
+          <p>Runtime: {movie.runtime} mins</p>
+          <p>Budget: ${movie.budget.toLocaleString()}</p>
+          <p>Revenue: ${movie.revenue.toLocaleString()}</p>
+        </div>
       </div>
-    </div>
-    <Trailer link={movie.trailer}/>
-    <div style={{display:"flex"}}>
-    <Music link={movie.music}/>
-    <Related movieId={movie._id}/>
-    </div>
+      <Trailer link={movie.trailer} />
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        <Music link={movie.music} />
+        <Related movieId={movie._id} />
+      </div>
     </div>
   );
 }
--- frontend/src/pages/MovieList.jsx
+++ frontend/src/pages/MovieList.jsx
@@ -1,25 +1,29 @@
 import { useState, useEffect } from "react";
 import SearchBar from "../components/SearchBar";
 import axios from "axios";
-import "./MovieDetails.css"
+import "./MovieDetails.css";
+
 export default function Home() {
   const [movies, setMovies] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredMovies, setFilteredMovies] = useState([]);
-  const [loading,setloading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // Fetch movies from the backend API
     axios
-      .get("https://marvel-w8vq.onrender.com/api/movies") //  backend API route
+      .get("https://marvel-w8vq.onrender.com/api/movies") // backend API route
       .then((response) => {
         setMovies(response.data); // Store all movies in state
         setFilteredMovies(response.data); // Initialize 
-        setloading(false);
+        setLoading(false);
       })
-      .catch((err) => {console.error("Error fetching movies:", err);
-      setloading(false);})
+      .catch((err) => {
+        console.error("Error fetching movies:", err);
+        setLoading(false);
+      });
   }, []);
+
   const handleSearch = (e) => {
     const query = e.target.value.toLowerCase();
     setSearchQuery(query);
@@ -32,11 +36,11 @@ export default function Home() {
   };
 
   return (
-    <div style={{backgroundColor:"black",paddingBottom:"20px",minHeight:"110vh"}}>
+    <div style={{ backgroundColor: "black", paddingBottom: "20px", minHeight: "110vh" }}>
       {/* Search Bar */}
-      <div style={{ textAlign: "center", padding: "10px",color:"white",marginBottom:"10px"}}>
-      <span style={{fontSize:"80px",margin:"20px",color:"#e62429"}}>Marvel Movies</span><br/><br/>
-      <span>Discover the Marvel Cinematic Universe</span><br/><br/>
+      <div style={{ textAlign: "center", padding: "10px", color: "white", marginBottom: "10px" }}>
+        <span style={{ fontSize: "80px", margin: "20px", color: "#e62429" }}>Marvel Movies</span><br /><br />
+        <span>Discover the Marvel Cinematic Universe</span><br /><br />
         <input
           type="text"
           placeholder="Search for marvel movie..."
@@ -46,67 +50,81 @@ export default function Home() {
             padding: "10px",
             width: "100%",
             maxWidth: "700px",
-            backgroundColor:"black",
-            color:"white",
+            backgroundColor: "black",
+            color: "white",
             fontSize: "16px",
             border: "1px solid #ccc",
             borderRadius: "4px",
+            marginBottom: "20px"
           }}
-        />&nbsp;&nbsp;<i className="fa-solid fa-magnifying-glass"></i>
-        <br/>
+        />
+        <i className="fa-solid fa-magnifying-glass"></i>
       </div>
+
       {/* Movies List */}
       <div
         style={{
           display: "grid",
           gridTemplateColumns: "repeat(auto-fill, minmax(280px, 1fr))",
           gap: "20px",
-          marginLeft :"100px",
-          marginRight :"100px",
+          marginLeft: "10px",
+          marginRight: "10px",
         }}
       >
-        {loading?(
-           <div>
-           <img src="https://cdn.dribbble.com/users/29051/screenshots/1671982/loading-anim-dribbble.gif" style={{paddingLeft:"400px",paddingTop:"50px",height:"500px",width:"800px"}}/>
-           <p style={{color:"white",fontSize:"60px",marginLeft:"90%"}}>loading...</p>
-           </div>
-        ):(filteredMovies.length > 0 ? (
-          filteredMovies.map((movie) => (
-            <a className="image2" href={`/movies/${movie._id}`}
-              key={movie._id}
+        {loading ? (
+          <div>
+            <img
+              src="https://cdn.dribbble.com/users/29051/screenshots/1671982/loading-anim-dribbble.gif"
               style={{
-                border: "1px solid grey",
-                borderRadius: "15px",
-                padding: "10px",
-                backgroundColor:"#151515",
-                color:"white",
-                textDecoration:"none"
+                paddingLeft: "400px",
+                paddingTop: "50px",
+                height: "500px",
+                width: "800px"
               }}
-            >
-              <img
-                src={movie.poster_path}
-                alt={movie.title}
-                style={{
-                  width: "100%",
-                  textAlign:"center",
-                  height: "350px",
-                  objectFit:"fill",
-                  opacity:"1",
-                  borderRadius: "15px",
-                }}
-              />
-              <p className="text" style={{fontSize:"21px"}}>{movie.title}</p>
-              <p className="text" style={{color:"lightgrey"}}> 
-              {`${new Date(movie.release_date).toLocaleString('default', { month: 'long' })} ${new Date(movie.release_date).getFullYear()}`}
-              </p>
-            </a>
-          ))
+            />
+            <p style={{ color: "white", fontSize: "60px", marginLeft: "90%" }}>loading...</p>
+          </div>
         ) : (
-          <>
-          <p style={{color:"white",fontSize:"30px"}}>No movies found.</p>
-          <p style={{color:"white",fontSize:"30px"}}>Search the movie in <b>OTHERS</b> section</p>
-          </>
-        ))}
+          filteredMovies.length > 0 ? (
+            filteredMovies.map((movie) => (
+              <a
+                className="image2"
+                href={`/movies/${movie._id}`}
+                key={movie._id}
+                style={{
+                  border: "1px solid grey",
+                  borderRadius: "15px",
+                  padding: "10px",
+                  backgroundColor: "#151515",
+                  color: "white",
+                  textDecoration: "none",
+                  transition: "transform 0.3s ease"
+                }}
+              >
+                <img
+                  src={movie.poster_path}
+                  alt={movie.title}
+                  style={{
+                    width: "100%",
+                    height: "350px",
+                    objectFit: "fill",
+                    opacity: "1",
+                    borderRadius: "15px",
+                  }}
+                />
+                <p className="text" style={{ fontSize: "21px" }}>{movie.title}</p>
+                <p className="text" style={{ color: "lightgrey" }}>
+                  {`${new Date(movie.release_date).toLocaleString('default', { month: 'long' })} ${new Date(movie.release_date).getFullYear()}`}
+                </p>
+              </a>
+            ))
+          ) : (
+            <>
+              <p style={{ color: "white", fontSize: "30px" }}>No movies found.</p>
+              <p style={{ color: "white", fontSize: "30px" }}>Search the movie in <b>OTHERS</b> section</p>
+            </>
+          )
+        )}
       </div>
     </div>
   );
