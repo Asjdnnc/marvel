bf75d4d8c5bfd88c4df565c6ab3328921be0a493
Asjdnnc <aditya05yt@gmail.com>
2025-01-28 23:47:36 +0530

    responsive

--- frontend/src/pages/MovieList.jsx
+++ frontend/src/pages/MovieList.jsx
@@ -15,7 +15,7 @@ export default function Home() {
       .get("https://marvel-w8vq.onrender.com/api/movies") // backend API route
       .then((response) => {
         setMovies(response.data); // Store all movies in state
-        setFilteredMovies(response.data); // Initialize 
+        setFilteredMovies(response.data); // Initialize
         setLoading(false);
       })
       .catch((err) => {
@@ -39,8 +39,11 @@ export default function Home() {
     <div style={{ backgroundColor: "black", paddingBottom: "20px", minHeight: "110vh" }}>
       {/* Search Bar */}
       <div style={{ textAlign: "center", padding: "10px", color: "white", marginBottom: "10px" }}>
-        <span style={{ fontSize: "80px", margin: "20px", color: "#e62429" }}>Marvel Movies</span><br /><br />
-        <span>Discover the Marvel Cinematic Universe</span><br /><br />
+        <span style={{ fontSize: "80px", margin: "20px", color: "#e62429" }}>Marvel Movies</span>
+        <br />
+        <span>Discover the Marvel Cinematic Universe</span>
+        <br />
+        <br />
         <input
           type="text"
           placeholder="Search for marvel movie..."
@@ -55,10 +58,10 @@ export default function Home() {
             fontSize: "16px",
             border: "1px solid #ccc",
             borderRadius: "4px",
-            marginBottom: "20px"
           }}
         />
-        <i className="fa-solid fa-magnifying-glass"></i>
+        &nbsp;&nbsp;<i className="fa-solid fa-magnifying-glass"></i>
+        <br />
       </div>
 
       {/* Movies List */}
@@ -67,8 +70,10 @@ export default function Home() {
           display: "grid",
           gridTemplateColumns: "repeat(auto-fill, minmax(280px, 1fr))",
           gap: "20px",
-          marginLeft: "10px",
-          marginRight: "10px",
+          paddingLeft: "10px",
+          paddingRight: "10px",
+          maxWidth: "1200px", // Ensures the layout doesn't stretch too wide
+          margin: "0 auto", // Centers the content horizontally
         }}
       >
         {loading ? (
@@ -79,51 +84,53 @@ export default function Home() {
                 paddingLeft: "400px",
                 paddingTop: "50px",
                 height: "500px",
-                width: "800px"
+                width: "800px",
               }}
             />
             <p style={{ color: "white", fontSize: "60px", marginLeft: "90%" }}>loading...</p>
           </div>
-        ) : (
-          filteredMovies.length > 0 ? (
-            filteredMovies.map((movie) => (
-              <a
-                className="image2"
-                href={`/movies/${movie._id}`}
-                key={movie._id}
+        ) : filteredMovies.length > 0 ? (
+          filteredMovies.map((movie) => (
+            <a
+              className="image2"
+              href={`/movies/${movie._id}`}
+              key={movie._id}
+              style={{
+                border: "1px solid grey",
+                borderRadius: "15px",
+                padding: "10px",
+                backgroundColor: "#151515",
+                color: "white",
+                textDecoration: "none",
+              }}
+            >
+              <img
+                src={movie.poster_path}
+                alt={movie.title}
                 style={{
-                  border: "1px solid grey",
+                  width: "100%",
+                  textAlign: "center",
+                  height: "350px",
+                  objectFit: "fill",
+                  opacity: "1",
                   borderRadius: "15px",
-                  padding: "10px",
-                  backgroundColor: "#151515",
-                  color: "white",
-                  textDecoration: "none",
-                  transition: "transform 0.3s ease"
                 }}
-              >
-                <img
-                  src={movie.poster_path}
-                  alt={movie.title}
-                  style={{
-                    width: "100%",
-                    height: "350px",
-                    objectFit: "fill",
-                    opacity: "1",
-                    borderRadius: "15px",
-                  }}
-                />
-                <p className="text" style={{ fontSize: "21px" }}>{movie.title}</p>
-                <p className="text" style={{ color: "lightgrey" }}>
-                  {`${new Date(movie.release_date).toLocaleString('default', { month: 'long' })} ${new Date(movie.release_date).getFullYear()}`}
-                </p>
-              </a>
-            ))
-          ) : (
-            <>
-              <p style={{ color: "white", fontSize: "30px" }}>No movies found.</p>
-              <p style={{ color: "white", fontSize: "30px" }}>Search the movie in <b>OTHERS</b> section</p>
-            </>
-          )
+              />
+              <p className="text" style={{ fontSize: "21px" }}>
+                {movie.title}
+              </p>
+              <p className="text" style={{ color: "lightgrey" }}>
+                {`${new Date(movie.release_date).toLocaleString("default", { month: "long" })} ${new Date(movie.release_date).getFullYear()}`}
+              </p>
+            </a>
+          ))
+        ) : (
+          <>
+            <p style={{ color: "white", fontSize: "30px" }}>No movies found.</p>
+            <p style={{ color: "white", fontSize: "30px" }}>
+              Search the movie in <b>OTHERS</b> section
+            </p>
+          </>
         )}
       </div>
     </div>
